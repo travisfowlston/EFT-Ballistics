3edb5f077421c4ac430ed609eddcfd6f6b470bc4
Yemi Ayeni <142445854+rehposolihpedoc@users.noreply.github.com>
2024-03-05 19:14:50 -0800

    Update footer index.jsx

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,23 +1,95 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Container,
+  Button,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "../../../src/App.css";
 
-const Footer = () => {
+// const imgStyle = {
+//   width: "40px",
+//   height: "40px",
+// };
+
+export default function Footer() {
+  const [footerStyle, setFooterStyle] = useState({});
   const location = useLocation();
-  const navigate = useNavigate();
-  return (
-    <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
-        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
-      </div>
-    </footer>
-  );
-};
+  // in use effect
+  useEffect(() => {
+    setTimeout(() => {
+      // obtain height of main container
+      const mainContainer = document.querySelector(".container");
+      const height = mainContainer.clientHeight;
+      console.log(height);
+      // if > 700px
+      if (height < 700) {
+        setFooterStyle({
+          position: "fixed",
+          width: "100%",
+          bottom: 0,
+        });
+      } else {
+        setFooterStyle({});
+      }
+      // then do nothing
+      // else apply 'fixed ' styling to footer
+    }, 100);
+  }, [location.pathname]);
 
-export default Footer;
+  return (
+    <Box
+      style={footerStyle}
+      bg={useColorModeValue("gray.200", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Box
+        borderTopWidth={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Container
+          as={Stack}
+          maxW={"6xl"}
+          py={4}
+          direction={{ base: "column", md: "row" }}
+          spacing={4}
+          justify={{ base: "center", md: "space-between" }}
+          align={{ base: "center", md: "center" }}
+        >
+          <Text fontSize="18" fontWeight="700" color="#DBC59c">
+            © 2024 EFT Ballistics
+          </Text>
+          <Stack direction={"row"} spacing={6}>
+            <div className="list-inline-item">
+              <a href="https://github.com/travisfowlston/EFT-Ballistics">
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="2x"
+                  style={{ color: "#DBC59c" }}
+                  className="icon z-2"
+                />
+              </a>
+            </div>
+            <div className="list-inline-item">
+              <a href="https://www.linkedin.com/">
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  size="2x"
+                  style={{ color: "#DBC59c" }}
+                  className="icon z-2"
+                />
+              </a>
+            </div>
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
