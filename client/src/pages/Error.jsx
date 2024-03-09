65ac8cf7419e5a252dd42cd77312e14d8e273b82
Travis Fowlston <travisfowlston@ymail.com>
2024-03-09 13:39:54 -0800

    added more to error page and README

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -5,12 +5,23 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <section className="container">
+      <div className="row justify-content-center mt-5 p-4">
+        <div className="col-md-6 text-center">
+          <img
+            src="https://placekitten.com/400/300"
+            alt="Page Not Found"
+            className="img-fluid"
+          />
+          <h1 className="mt-4">Uh oh, something went wrong!</h1>
+          <p className="lead mt-4">
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <a class="btn btn-primary" href="/" role="button">
+            Go Home!
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
