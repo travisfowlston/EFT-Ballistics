8ca0bf6eea587123fb1bdc737108c62ab4a236f6
Travis Fowlston <travisfowlston@ymail.com>
2024-03-11 23:22:38 -0700

    added stuff to deploy to render and atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/eft-ballistics');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/eft-ballistics"
+);
 
 module.exports = mongoose.connection;
