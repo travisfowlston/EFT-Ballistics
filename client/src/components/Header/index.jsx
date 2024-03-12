e28afdf15eb6c0f038d0a8025257ae70de4911a7
neilsen zulueta <neilzulueta@gmail.com>
2024-03-12 15:31:51 -0700

    updated header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -8,33 +8,40 @@ const Header = () => {
   };
 
   return (
-    <header className="header-bg">
-      <div className="container d-flex justify-content-between align-items-center h-100">
-        <RouterLink to="/">
-          <img
-            src="./src/assets/logo.png"
-            alt="EFT-BALLISTICS Logo"
-            className="logo"
-          />
-        </RouterLink>
-        <div className="header-links">
-          {Auth.loggedIn() ? (
-            <>
-              <RouterLink to="/me" className="btn custom-btn me-2">View My Profile</RouterLink>
-              <button onClick={logout} className="btn custom-btn">Logout</button>
-            </>
-          ) : (
-            <>
-              <RouterLink to="/login" className="btn custom-btn me-2">Login</RouterLink>
-              <RouterLink to="/signup" className="btn custom-btn">Signup</RouterLink>
-            </>
-          )}
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container">
+          <RouterLink to="/" className="navbar-brand">EFT-BALLISTICS</RouterLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              {Auth.loggedIn() ? (
+                <>
+                  <li className="nav-item">
+                    <RouterLink to="/me" className="nav-link btn custom-btn-nav">View My Profile</RouterLink>
+                  </li>
+                  <li className="nav-item">
+                    <button onClick={logout} className="nav-link btn custom-btn-nav">Logout</button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <RouterLink to="/login" className="nav-link btn custom-btn-nav">Login</RouterLink>
+                  </li>
+                  <li className="nav-item">
+                    <RouterLink to="/signup" className="nav-link btn custom-btn-nav">Signup</RouterLink>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
 
 export default Header;
-
-
